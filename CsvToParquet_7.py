400803a5f0bcbcebd8bf5983673f7a8a123d676e
Ashish Kumar <71031421+AshishSaw@users.noreply.github.com>
2021-06-09 20:03:28 +0530

    Add files via upload

---
+++ CsvToParquet_7.py
@@ -0,0 +1,19 @@
+
+import SparkConf
+import SparkContext
+
+def connect_azure_blob(storage_account_name, blob_storage, storage_account_key, container_name, folder_name, file_type):
+
+
+    file_location = "wasbs://" + container_name + "@" + storage_account_name + ".blob.core.windows.net/" + folder_name
+
+    #tempDir = "wasbs://" + container_name + "@" + storage_account_name + ".blob.core.windows.net/tempDir"
+
+    #spark.conf.set("fs.azure.account.key." + storage_account_name + ".blob.core.windows.net", storage_account_key)
+    #dbutils.fs.ls(file_location)
+    #spark.conf.set('fs.azure.account.key.' + storage_account_name + '.blob.core.windows.net', storage_account_access_key)
+
+    sparkConfig = 'fs.azure.account.key.' + storage_account_name + '.blob.core.windows.net' + storage_account_access_key
+
+    return file_location
+    return sparkConfig
