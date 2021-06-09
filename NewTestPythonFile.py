522698a08e1ef1700514b8fd356405b1bdf3a3ec
Ashish Kumar <71031421+AshishSaw@users.noreply.github.com>
2021-06-09 15:48:25 +0530

    Add files via upload

---
+++ NewTestPythonFile.py
@@ -0,0 +1,19 @@
+def addnum(num1, num2):
+    output = num1 + num2
+    print(output)
+
+
+def printnum(num):
+    while num <= 100:
+        print(num)
+        num = num + 1
+
+
+def addnewnum(a,b,c):
+    output = a + b + c
+    print(output)
+
+
+
+
+
